e66b9f5b1d6832917cfda5e9029bd295f13a4d45
Rkozik <rkozik1989@gmail.com>
2020-04-29 11:07:54 -0500

    added pathing support for basements.

--- WalledCity/unit.js
+++ WalledCity/unit.js
@@ -98,11 +98,20 @@ class Unit{
     }
 
     checkLobbyConnection(){
+        // Above ground units
         if(this.node.type !== null && this.node.type !== "lobby" &&
                 (this.neighbors.westernNeighbor().type === "balcony" ||
                     this.neighbors.westernNeighbor().type === "clothes-line")){
 
-            let lobby = this.traverseToLobby(this.node);
+            let lobby = this.traverseToLobby(this.node, "south");
+            let route = new Route(this.tower);
+            return route.traverse(this.node, lobby).length > 0;
+        }
+
+        // Below ground units
+        if(this.node.type !== null && this.node.type !== "lobby" &&
+            this.neighbors.westernNeighbor().type === "basement"){
+            let lobby = this.traverseToLobby(this.node, "north");
             let route = new Route(this.tower);
             return route.traverse(this.node, lobby).length > 0;
         }
@@ -111,7 +120,7 @@ class Unit{
 
     warnRow(node){
         let neighbors = new Neighbor(node, this.tower);
-        if(node.type === "balcony" || node.type === "clothes-line"){
+        if(node.type === "balcony" || node.type === "clothes-line" || node.type === "basement"){
             return true;
         }
 
@@ -124,7 +133,7 @@ class Unit{
 
     }
 
-    traverseToLobby(node){
+    traverseToLobby(node, direction){
         let neighbors = new Neighbor(node, this.tower);
 
         if(neighbors.westernNeighbor().type === "entrance"){
@@ -133,11 +142,15 @@ class Unit{
 
         if(node.floor_id === 6){
             if(neighbors.westernNeighbor() !== "entrance"){
-                return this.traverseToLobby(neighbors.westernNeighbor());
+                return this.traverseToLobby(neighbors.westernNeighbor(), direction);
             }
         }
 
-        return this.traverseToLobby(neighbors.southernNeighbor());
+        if(direction === "south"){
+            return this.traverseToLobby(neighbors.southernNeighbor(), direction);
+        } else {
+            return this.traverseToLobby(neighbors.northernNeighbor(), direction);
+        }
     }
 
     canConstructAboveGround(){
